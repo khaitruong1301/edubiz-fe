845630e01612a44914b5f5f03719a79be1b8f9e7
unknown <tienhoang1211@gmail.com>
2023-05-12 16:48:40 +0700

    hoang 12-05-2023 v2

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 const environment = {
-  baseUrl: "https://localhost:5001",
-  // baseUrl: "https://backend.edubiz.vn",
+  // baseUrl: "https://localhost:5001",
+  baseUrl: "https://backend.edubiz.vn",
   faceBookAppId: "130543658347826",
   env: "dev",
   production: false,
